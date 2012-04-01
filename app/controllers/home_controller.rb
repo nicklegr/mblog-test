2b5a8eec7c71697b2249ad3637a78d5bfecb6024
Nickle <nickle-github@nickle.ath.cx>
2012-04-01 14:18:53 +0900

    initial import

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  def index
+    @posts = Post.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @posts }
+    end
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mblog::Application.config.secret_token = 'c0809d71ee49815f189af422c92c7d6618a67375bcc224df69bef1e86c5978d5badc1e0b7bae5428319cc2f04f07b904f99246c0588c760f02c683f58d0a81f4'
